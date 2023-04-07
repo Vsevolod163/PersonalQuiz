686554f4e51126129e38627c47da61855b218c07
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-07 14:59:37 +0300

    func getResultAnimal func changed

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -34,57 +34,35 @@ final class ResultViewController: UIViewController {
     }
     
     private func getResultAnimalIn(answers: [Answer]) -> Animal {
-//        var dogAnswer = 0
-//        var catAnswer = 0
-//        var rabbitAnswer = 0
-//        var turtleAnswer = 0
-//
-//        for answer in answers {
-//            switch answer.animal {
-//            case .dog:
-//                dogAnswer += 1
-//            case .cat:
-//                catAnswer += 1
-//            case .rabbit:
-//                rabbitAnswer += 1
-//            case .turtle:
-//                turtleAnswer += 1
-//            }
-//        }
-//
-//        let animalsAnswers = [dogAnswer, catAnswer, rabbitAnswer, turtleAnswer]
-//        let maximumElemet = animalsAnswers.max() ?? 0
-//        let indexOfMaxElement = animalsAnswers.firstIndex(of: maximumElemet)
-//
-//        switch indexOfMaxElement {
-//        case 0:
-//            return Animal.dog
-//        case 1:
-//            return Animal.cat
-//        case 2:
-//            return Animal.rabbit
-//        case 3:
-//            return Animal.turtle
-//        default:
-//            return Animal.dog
-//        }
-        var animals: [Animal: Int] = [:]
-        var resultAnimal: Animal!
-        
+        var animalsCount: [Int] = Array(repeating: 0, count: 4)
+
         for answer in answers {
-            if let count = animals[answer.animal] {
-                animals[answer.animal] = count + 1
-            } else {
-                animals[answer.animal] = 1
+            switch answer.animal {
+            case .dog:
+                animalsCount[0] += 1
+            case .cat:
+                animalsCount[1] += 1
+            case .rabbit:
+                animalsCount[2] += 1
+            case .turtle:
+                animalsCount[3] += 1
             }
         }
         
-        let maxCount = animals.values.max()
-        if let mostCommonAnimal = animals.first(where: { $0.value == maxCount })?.key {
-            resultAnimal = mostCommonAnimal
-        }
-        
-        return resultAnimal
-    }
+        let maximumElemet = animalsCount.max() ?? 0
+        let indexOfMaxElement = animalsCount.firstIndex(of: maximumElemet)
 
+        switch indexOfMaxElement {
+        case 0:
+            return Animal.dog
+        case 1:
+            return Animal.cat
+        case 2:
+            return Animal.rabbit
+        case 3:
+            return Animal.turtle
+        default:
+            return Animal.dog
+        }
+    }
 }
