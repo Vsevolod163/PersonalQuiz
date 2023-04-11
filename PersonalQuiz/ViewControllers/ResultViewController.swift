c430588c890e1491c7c22c9092ceca5437f4208d
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-11 16:24:28 +0300

    Dop  variants of func getResultAnimal added

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -17,8 +17,11 @@ final class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
-        
-        getResultAnimal()
+        // Сalcualte time
+        let time = ContinuousClock().measure {
+            getResultAnimal()
+        }
+        print(time)
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
@@ -34,7 +37,7 @@ extension ResultViewController {
     private func getResultAnimal() {
         var frequencyOfAnimals: [Animal: Int] = [:]
         let animals = answersChosen.map { $0.animal }
-        
+
         for animal in animals {
             if let animalTypeCount = frequencyOfAnimals[animal] {
                 frequencyOfAnimals.updateValue(animalTypeCount + 1, forKey: animal)
@@ -42,9 +45,22 @@ extension ResultViewController {
                 frequencyOfAnimals.updateValue(1, forKey: animal)
             }
         }
-        
+
         let sortedFrequentOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value }
         guard let mostFrequentAnimal = sortedFrequentOfAnimals.first?.key else { return }
+        
+        // 2 Вариат
+//        for animal in animals {
+//            frequencyOfAnimals[animal] = (frequencyOfAnimals[animal] ?? 0) + 1
+//        }
+        // 3 Вариант
+//        for animal in animals {
+//            frequencyOfAnimals[animal, default: 0] += 1
+//        }
+        // 4 Вариант
+//        let mostFrequencyAnimal = Dictionary(grouping: answersChosen) { $0.animal }
+//            .sorted { $0.value.count > $1.value.count }
+//            .first?.key
         
         updateUI(with: mostFrequentAnimal)
     }
